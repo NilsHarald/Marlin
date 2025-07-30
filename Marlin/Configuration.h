d11b40eddae9291847f15760c43b22c2373cc507
NilsHarald <87765734+NilsHarald@users.noreply.github.com>
2025-07-30 21:24:06 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1498,7 +1498,7 @@
  *    - Normally-closed (NC) also connect to GND.
  *    - Normally-open (NO) also connect to 5V.
  */
-//#define Z_MIN_PROBE_PIN PC14
+#define Z_MIN_PROBE_PIN PC14
 
 /**
  * Probe Type
