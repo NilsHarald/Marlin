e7f3cd85feffd9d5dd0ffd749deab82d70d2c349
NilsHarald <87765734+NilsHarald@users.noreply.github.com>
2025-08-02 12:18:22 +0200

    update

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -691,7 +691,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN 44 // Turn off extruder fan when its temperature is above 50°C
+#define E0_AUTO_FAN_PIN -1 // Turn off extruder fan when its temperature is above 50°C
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
